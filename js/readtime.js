e9716e9c7b86e9e699fa95576817e97f923b8f7b
Christine Wilks <christine@Christines-MacBook-Pro.local>
2013-11-17 17:58:20 +0000

    Moved tweening to new doTween function

--- js/readtime.js
+++ js/readtime.js
@@ -43,16 +43,22 @@ function startAnimation(){
         if (isSentenceEnd) {
           duration += 0.6; //if it's the last word in a sentence, drag out the timing a bit for a dramatic pause.
         }
-        //set opacity and scale to 0 initially. We set z to 0.01 just to kick in 3D rendering in the browser which makes things render a bit more smoothly.
-      TweenLite.set(element, {autoAlpha:0, scale:0, z:0.01});
-      //the SlowMo ease is like an easeOutIn but it's configurable in terms of strength and how long the slope is linear. See http://www.greensock.com/v12/#slowmo and http://api.greensock.com/js/com/greensock/easing/SlowMo.html
-      tl.to(element, duration, {scale:2,  ease:SlowMo.ease.config(0.25, 0.9)}, time)
-        //notice the 3rd parameter of the SlowMo config is true in the following tween - that causes it to yoyo, meaning opacity (autoAlpha) will go up to 1 during the tween, and then back down to 0 at the end. 
-  		 	  .to(element, duration, {autoAlpha:1, ease:SlowMo.ease.config(0.25, 0.9, true)}, time);
+
+        doTween();
+
         time += duration - 0.05;
         if (isSentenceEnd) {
           time += 0.6; //at the end of a sentence, add a pause for dramatic effect.
         }
+      }
+
+      function doTween(){
+        //set opacity and scale to 0 initially. We set z to 0.01 just to kick in 3D rendering in the browser which makes things render a bit more smoothly.
+      TweenLite.set(element, {autoAlpha:0, scale:0, z:0.01});
+      //the SlowMo ease is like an easeOutIn but it's configurable in terms of strength and how long the slope is linear. See http://www.greensock.com/v12/#slowmo and http://api.greensock.com/js/com/greensock/easing/SlowMo.html
+      tl.to(element, duration, {scale:1.5,  ease:SlowMo.ease.config(0.25, 0.9)}, time)
+        //notice the 3rd parameter of the SlowMo config is true in the following tween - that causes it to yoyo, meaning opacity (autoAlpha) will go up to 1 during the tween, and then back down to 0 at the end. 
+          .to(element, duration, {autoAlpha:1, ease:SlowMo.ease.config(0.25, 0.9, true)}, time);
       }
   }
 
